e31878a03f9acd77d309aa53b209a3f8eca58ef7
Ilya V. Schurov <ilya@schurov.com>
2016-01-31 23:26:07 +0300

    Basic IndexedList added (with some tests)

---
+++ qqmbr/indexedlist.py
@@ -0,0 +1,112 @@
+from collections import MutableSequence, Sequence, Mapping
+from sortedcontainers import SortedList
+
+
+class IndexedList(MutableSequence):
+
+    def __init__(self, *iterable):
+        if len(iterable) == 1 and isinstance(iterable[0], Sequence):
+            iterable = iterable[0]
+        self._container = list(iterable)
+        self._locator = {}
+        self.update_index()
+
+    def __delitem__(self, i):
+        old_element = self._container[i]
+        self._locator[self.get_key(old_element)].remove(i)
+        for key, places in self._locator.items():
+            for k, index in enumerate(places):
+                if index >= i:
+                    places[k] -= 1
+
+        del self._container[i]
+
+    def __getitem__(self, i):
+        return self._container[i]
+
+    def __len__(self):
+        return len(self._container)
+
+    def __setitem__(self, i, item):
+        places = self._locator[self.get_key(self._container[i])]
+        places.remove(i)
+
+        self._container[i] = item
+
+        self.add_index(i, item)
+
+    def insert(self, i, x):
+        for key, places in self._locator.items():
+            for k in range(len(places)-1, -1, -1):
+                if places[k] >= i:
+                    places[k] += 1
+                else:
+                    break
+        self._container.insert(i, x)
+        self.add_index(i, x)
+
+    def __str__(self):
+        return str(self._container)
+
+    def __repr__(self):
+        return "IndexedList(%s)" % repr(self._container)
+
+    def find_index(self, key):
+        return self._locator[key][0]
+
+    def find_all_index(self, key):
+        return self._locator[key]
+
+    def find_all(self, key):
+        return [self._container[i] for i in self.find_all_index(key)]
+
+    def find(self, key):
+        return self._container[self.find_index(key)]
+
+    def update_index(self):
+        self._locator.clear()
+        for i, item in enumerate(self._container):
+            self.add_index(i, item)
+
+    def add_index(self, i, item):
+        key = self.get_key(item)
+        if key not in self._locator:
+            self._locator[key] = SortedList()
+        self._locator[key].add(i)
+
+    def is_consistent(self):
+        for i, el in enumerate(self._container):
+            if i not in self.find_all_index(self.get_key(el)):
+                return False
+        return True
+
+    def __eq__(self, other):
+        if isinstance(other, IndexedList):
+            return self._container == other._container
+        elif isinstance(other, Sequence):
+            return self._container == other
+        else:
+            return False
+
+    def get_key(self, item):
+        if isinstance(item, str):
+            return str
+        elif isinstance(item, Sequence):
+            if item:
+                return item[0]
+            else:
+                return None
+        elif isinstance(item, Mapping):
+            if len(item) == 1:
+                return list(item)[0]
+            else:
+                return Mapping
+        else:
+            try:
+                ret = item.__qqkey__()
+            except AttributeError:
+                ret = str
+        return ret
+
+    
+
---
+++ test/test_indexedlist.py
@@ -0,0 +1,104 @@
+import unittest
+from qqmbr.indexedlist import IndexedList
+from sortedcontainers import SortedList
+
+class TestIndexedlistMethods(unittest.TestCase):
+
+    def test_creating_indexedlist1(self):
+        q = IndexedList([['a', 'b'], ['a', 'd']])
+        self.assertEqual(list(q._locator['a']), [0, 1])
+        self.assertTrue(q.is_consistent())
+
+
+    def test_creating_indexedlist2(self):
+        q = IndexedList(['a', 'b'], {'a': 123}, 'a', 123, ['a', 'b', 'c'], ['b', 123], ['a'], {'b': 321})
+        self.assertEqual(repr(q), "IndexedList([['a', 'b'], {'a': 123}, 'a', 123, ['a', 'b', 'c'], ['b', 123], ['a'], {'b': 321}])")
+        self.assertEqual(eval(repr(q)), q)
+        self.assertEqual(q._locator, {'b': SortedList([5, 7]),
+                                   'a': SortedList([0, 1, 4, 6]),
+                                      str: SortedList([2, 3])})
+        self.assertTrue(q.is_consistent())
+
+    def test_delitem(self):
+        q = IndexedList(['a', 'b'], {'a': 123}, 'a', 123, ['a', 'b', 'c'], ['b', 123], ['a'], {'b': 321})
+
+        del q[0]
+        self.assertEqual(q._locator,
+                         {'b': SortedList([4, 6]),
+                          'a': SortedList([0, 3, 5]),
+                          str: SortedList([1, 2])})
+        self.assertTrue(q.is_consistent())
+
+        del q[2]
+        self.assertEqual(q._locator,
+                         {'b': SortedList([3, 5]),
+                         'a': SortedList([0, 2, 4]),
+                         str: SortedList([1])})
+        self.assertTrue(q.is_consistent())
+
+        del q[3]
+        self.assertEqual(q._locator,
+                         {'b': SortedList([4]),
+                         'a': SortedList([0, 2, 3]),
+                         str: SortedList([1])})
+
+        self.assertTrue(q.is_consistent())
+
+        del q[0]
+        self.assertTrue(q.is_consistent())
+
+        del q[3]
+        self.assertTrue(q.is_consistent())
+
+        del q[0]
+        self.assertTrue(q.is_consistent())
+
+        del q[1]
+        self.assertTrue(q.is_consistent())
+
+        del q[0]
+        self.assertTrue(q.is_consistent())
+        self.assertEqual(q, [])
+
+    def test_setitem(self):
+        q = IndexedList(['a', 'b'], {'a': 123}, 'a', 123, ['a', 'b', 'c'], ['b', 123], ['a'], {'b': 321})
+        q[0] = 2
+        self.assertEqual(q._locator,
+                         {'b': SortedList([5, 7]),
+                         'a': SortedList([1, 4, 6]),
+                         str: SortedList([0, 2, 3])})
+
+        self.assertTrue(q.is_consistent())
+
+        q[2] = ['b', 'c', 'd']
+        self.assertEqual(q._locator,
+                         {'b': SortedList([2, 5, 7]),
+                         'a': SortedList([1, 4, 6]),
+                         str: SortedList([0, 3])})
+        self.assertTrue(q.is_consistent())
+
+        q[1] = ['cd', 'efg', 12]
+        self.assertEqual(q._locator,
+                         {'a': SortedList([4, 6]),
+                         'b': SortedList([2, 5, 7]),
+                         'cd': SortedList([1]),
+                         str: SortedList([0, 3])})
+        self.assertTrue(q.is_consistent())
+
+    def test_insert(self):
+        q = IndexedList(['a', 'b'], {'a': 123}, 'a', 123, ['a', 'b', 'c'], ['b', 123], ['a'], {'b': 321})
+        q.insert(2, 'b')
+        self.assertEqual(q._locator, {'a': SortedList([0, 1, 5, 7]),
+                                     'b': SortedList([6, 8]),
+                                      str: SortedList([2, 3, 4])})
+        self.assertTrue(q.is_consistent())
+
+        q.insert(0, ['b', 123])
+        self.assertEqual(q._locator, {'a': SortedList([1, 2, 6, 8]),
+                                     'b': SortedList([0, 7, 9]),
+                                      str: SortedList([3, 4, 5])})
+        self.assertTrue(q.is_consistent())
+
+
+if __name__ == '__main__':
+    unittest.main()
