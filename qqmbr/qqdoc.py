cf0affe7309894101240de86491677f1a974b814
Ilya V. Schurov <ilya@schurov.com>
2016-02-01 14:13:57 +0300

    Inline tags

--- qqmbr/qqdoc.py
+++ qqmbr/qqdoc.py
@@ -18,18 +18,24 @@ class QqTag(object):
             self.children = IndexedList()
         elif isinstance(children, str) or isinstance(children, int) or isinstance(children, float):
             self.children = IndexedList([children])
-        elif isinstance(children, Sequence) and type(children) != IndexedList:
+        elif isinstance(children, Sequence):
             self.children = IndexedList(children)
         else:
             raise QqError("I don't know what to do with children " + str(children))
 
     def __repr__(self):
-        return "QqTag(%s, %s)" % (repr(self.name), self.children)
+        if self.parent is None:
+            return "QqTag(%s, %s)" % (repr(self.name), repr(self.children))
+        else:
+            return "QqTag(%s, %s, parent = %s)" % (repr(self.name), repr(self.children), repr(self.parent))
+
 
     def __str__(self):
         return "{%s : %s}" % (self.name, self.children)
 
     def __eq__(self, other):
+        if other is None:
+            return False
         return self.__dict__ == other.__dict__
 
     @property
@@ -73,21 +79,68 @@ class QqTag(object):
     def __call__(self, key):
         return self.find_all(key)
 
+    def as_list(self):
+        ret = [self.name]
+        for child in self.children:
+            if isinstance(child, QqTag):
+                ret.append(child.as_list())
+            else:
+                ret.append(child)
+        return ret
+
+    def append_child(self, child):
+        self.children.append(child)
+        child.parent = self
+
+    def append_line(self, line):
+        if line:
+            self.children.append(line)
+
+    def __getitem__(self, item):
+        return self.children[item]
+
+class StackElement(object):
+    def __init__(self, tag, indent=0, bracket=None, bracket_counter=0):
+        self.tag = tag
+        self.indent = indent
+        self.bracket = bracket
+        self.bracket_counter = bracket_counter
+    def __str__(self):
+        return "<%s, %s, %s, %s>" % (self.tag.name, str(self.indent), self.bracket, str(self.bracket_counter))
+    def __repr__(self):
+        return "StackElement(%s, %s, %s, %s)" % (repr(self.tag),
+                                                 repr(self.indent),
+                                                 repr(self.bracket),
+                                                 repr(self.bracket_counter))
+
 class QqParser(object):
-    def __init__(self, command_symbol='\\', command_regexp=r'\\', allowed_tags=None):
+    def __init__(self, command_symbol='\\', command_regex=r'\\',
+                 allowed_tags=None, tag_regex=r"(\w+)", allowed_inline_tags=None):
         self.command_symbol = command_symbol
-        self.command_regexp = command_regexp
+        self.command_regex = command_regex
         if allowed_tags is None:
             self.allowed_tags = {}
         else:
             self.allowed_tags = allowed_tags
+        self.tag_regex = tag_regex
+        if allowed_inline_tags is None:
+            self.allowed_inline_tags = self.allowed_tags
+        else:
+            self.allowed_inline_tags = allowed_inline_tags
+        self._last_tag = None
+        self._stack = None
+
+    def pop_stack(self):
+        self._last_tag = self._stack.pop()
+        return self._last_tag
 
     def get_indent(self, s):
         m = re.match(r'\s*', s)
         beginning = m.group(0)
         if '\t' in beginning:
             raise QqError("No tabs allowed in QqDoc at the beginning of line!")
-        return len(beginning)
+        m = re.match(r' *', s)
+        return len(m.group(0))
 
 
 
@@ -107,12 +160,18 @@ class QqParser(object):
             lines = lines.splitlines()
 
         tree = QqTag('_root')
-        StackElement = namedtuple('StackElement', ['tag', 'indent'])
         stack = [StackElement(tree, self.get_indent(lines[0])-1)]
+        self._stack = stack
+        self._last_tag = stack[0]
+        inline_mode = False
 
         current_indent = 0
 
         for i, line in enumerate(lines):
+
+
+            if not line or line[-1] != '\n':
+                line = line + "\n"
 
             indent_decreased = False
             indent = self.get_indent(line)
@@ -122,25 +181,33 @@ class QqParser(object):
                 indent_decreased = True
                 current_indent = indent
 
-            while indent <= stack[-1].indent:
-                last_tag = stack.pop()
+            if indent_decreased and inline_mode:
+                raise QqError("Indent decreased during inline mode on line %i: %s", (i + 1, lin))
 
-            if indent_decreased and indent != last_tag.indent:
+
+            if not inline_mode:
+                while indent <= stack[-1].indent:
+                    self.pop_stack()
+
+            if indent_decreased and indent != self._last_tag.indent:
                 raise QqError("Formatting error: unexpected indent on line %i: %s \n\
                 (expected indent %i on tag %s, get indent %i, stack: %s)" % (i + 1,
                                                                              lin,
-                                                                             last_tag.indent,
-                                                                             last_tag.tag.name, indent, str(stack)))
+                                                                             self._last_tag.indent,
+                                                                             self._last_tag.tag.name, indent, str(stack)))
 
             current_tag = stack[-1].tag
 
             if lin and lin[0] == self.command_symbol:
-                m = re.match(self.command_regexp+r"(\w+)", lin)
+                m = re.match(self.command_regex + self.tag_regex, lin)
                 if m:
                     tag = m.group(1)
                     if tag in self.allowed_tags:
-                        new_tag = QqTag(tag, [], parent=current_tag)
-                        current_tag.children.append(new_tag)
+                        if inline_mode:
+                            raise QqError("New block tag open during inline mode on line %i: %s", (i + 1, lin))
+
+                        new_tag = QqTag(tag, [])
+                        current_tag.append_child(new_tag)
                         stack.append(StackElement(new_tag, indent))
 
                         if i < len(lines)-1 and self.get_indent(lines[i+1]) > indent:
@@ -149,8 +216,54 @@ class QqParser(object):
                         continue
                         # TODO: extended syntax
 
-            # Ordinary line, not a tag
-            # Append it to current tag
-            current_tag.children.append(line[current_indent:])
+
+
+            inlines = re.finditer(self.command_regex +
+                                  r'(?P<tag>' + self.tag_regex + ')' +
+                                  r'(?P<bracket>[\{\[])' +
+                                  r"|[\{\[\]\}]", line)
+
+            # inline tags or brackets
+
+            cursor = current_indent
+
+            for m in inlines:
+                if inline_mode:
+                    if m.group(0) == stack[-1].bracket:
+                        stack[-1].bracket_counter += 1
+                    elif m.group(0) == {'{':'}', '[':']'}[stack[-1].bracket]:
+                        stack[-1].bracket_counter -= 1
+                        if stack[-1].bracket_counter == 0:
+                            # close current inline tag
+
+                            current_tag.append_line(line[cursor: m.start()])
+                            cursor = m.end()
+
+                            self.pop_stack()
+                            current_tag = stack[-1].tag
+                            if stack[-1].bracket is None:
+                                inline_mode = False
+
+                inline_tag = m.group('tag')
+                if inline_tag is not None:
+                    if inline_tag in self.allowed_inline_tags:
+                        inline_mode = True
+                        current_tag.append_line(line[cursor: m.start()])
+
+                        new_tag = QqTag(inline_tag, [])
+                        current_tag.append_child(new_tag)
+                        stack.append(StackElement(new_tag, indent=None,
+                                                  bracket=m.group('bracket'),
+                                                  bracket_counter=1))
+                        current_tag = new_tag
+                        cursor = m.end()
+
+                    else:
+                        if inline_mode and m.group(0) == stack[-1].bracket:
+                            stack[-1].bracket_counter += 1
+
+
+            # Append the rest of line to current tag
+            current_tag.append_line(line[cursor:])
 
         return tree
--- test/test_qqdoc.py
+++ test/test_qqdoc.py
@@ -1,7 +1,7 @@
 import unittest
 from qqmbr.indexedlist import IndexedList
 from sortedcontainers import SortedList
-from qqmbr.qqdoc import QqTag
+from qqmbr.qqdoc import QqTag, QqParser
 
 class TestQqTagMethods(unittest.TestCase):
     def test_create_qqtag(self):
@@ -21,6 +21,7 @@ class TestQqTagMethods(unittest.TestCase):
         self.assertEqual(q.name, 'a')
         self.assertEqual(q.children, IndexedList([QqTag('b', ['hello']), QqTag('c', ['world']), QqTag('b', ['this']), QqTag('--+-', [QqTag('b', ['way']), 'this'])]))
         self.assertEqual(eval(repr(q)), q)
+        self.assertEqual(q.as_list(), ['a', ['b', 'hello'],['c', 'world'], ['b', 'this'], ['--+-', ['b', 'way'], 'this']])
 
     def test_qqtag_accessors(self):
         q = QqTag('a', [
@@ -36,3 +37,155 @@ class TestQqTagMethods(unittest.TestCase):
         self.assertEqual(q._c.value, 'world')
         self.assertEqual(q('b'), [QqTag('b', 'hello'), QqTag('b', 'this')])
         self.assertEqual(q.find('--+-')._b.value, 'way')
+        self.assertEqual(q[0].value, 'hello')
+        self.assertEqual(q[1].value, 'world')
+        self.assertEqual(q[3][0].value, 'way')
+
+
+class TestQqParser(unittest.TestCase):
+    def test_block_tags1(self):
+        doc = r"""Hello
+\tag
+    World
+"""
+        parser = QqParser(allowed_tags={'tag'})
+        tree = parser.parse(doc)
+        self.assertEqual(tree[0], "Hello\n")
+
+        self.assertEqual(tree._tag.name, 'tag')
+        self.assertEqual(tree._tag.value, 'World\n')
+
+    def test_block_tags_nested(self):
+        doc = r"""Hello
+\tag
+    World
+    \othertag
+        This
+        Is
+    A test
+The end
+
+Blank line before the end
+"""
+        parser = QqParser(allowed_tags={'tag', 'othertag'})
+        tree = parser.parse(doc)
+        self.assertEqual(tree[0], "Hello\n")
+        self.assertEqual(tree._tag[0], "World\n")
+        self.assertEqual(tree._tag._othertag.children, ["This\n", "Is\n"])
+        self.assertEqual(tree._tag[2], 'A test\n')
+        self.assertEqual(tree[2], 'The end\n')
+        self.assertEqual(tree[3], '\n')
+        self.assertEqual(tree[4], 'Blank line before the end\n')
+        self.assertEqual(tree._tag.parent, tree)
+        self.assertEqual(tree._tag._othertag.parent, tree._tag)
+
+    def test_block_additional_indent(self):
+        doc = r"""Hello
+\tag
+    First
+        Second
+    Third
+End"""
+        parser = QqParser(allowed_tags={'tag'})
+        tree = parser.parse(doc)
+        self.assertEqual(tree._tag.children, ['First\n', '    Second\n', 'Third\n'])
+
+    def test_inline_tag1(self):
+        doc = r"""Hello, \tag{inline} tag!
+"""
+        parser = QqParser(allowed_tags={'tag'})
+        tree = parser.parse(doc)
+        self.assertEqual(tree[0], 'Hello, ')
+        self.assertEqual(tree._tag.value, 'inline')
+        self.assertEqual(tree[2], ' tag!\n')
+
+    def test_inline_tag2(self):
+        doc = r"""Hello, \othertag{\tag{inline} tag}!
+"""
+        parser = QqParser(allowed_tags={'tag', 'othertag'})
+        tree = parser.parse(doc)
+        self.assertEqual(tree._othertag._tag.value, 'inline')
+        self.assertEqual(tree.as_list(), ['_root', 'Hello, ', ['othertag', ['tag', 'inline'], ' tag'], '!\n'])
+
+    def test_inline_tag3(self):
+        doc = r"""Hello, \tag{
+this is a continuation of inline tag on the next line
+
+the next one\othertag{okay}}
+"""
+        parser = QqParser(allowed_tags={'tag', 'othertag'})
+        tree = parser.parse(doc)
+        self.assertEqual(tree.as_list(),[
+            '_root', 'Hello, ',
+            [
+                'tag',
+                '\n',
+                'this is a continuation of inline tag on the next line\n',
+                '\n',
+                'the next one',
+                [
+                    'othertag',
+                    'okay'
+                ]
+            ],
+            '\n'
+        ])
+
+    def test_inline_tag4(self):
+        doc = r"Hello, \tag{I'm [your{taggy}] tag} okay"
+        parser = QqParser(allowed_tags={'tag', 'othertag'})
+        tree = parser.parse(doc)
+        self.assertEqual(tree.as_list(),[
+            '_root', 'Hello, ',
+            [
+                'tag',
+                "I'm [your{taggy}] tag"
+            ],
+            " okay\n"
+        ])
+
+    def test_block_and_inline_tags(self):
+        doc = r"""Hello,
+\tag
+    I'm your \othertag{tag}
+    \tag
+        {
+        \tag
+            {
+            this \tag{is a {a test}
+            okay}
+        }
+    }
+"""
+        parser = QqParser(allowed_tags={'tag', 'othertag'})
+        tree = parser.parse(doc)
+        self.assertEqual(tree.as_list(),[
+            '_root', 'Hello,\n',
+            [
+                'tag',
+                "I'm your ",
+                ['othertag', 'tag'],
+                '\n',
+                [
+                    'tag',
+                    '{\n',
+                    [
+                        'tag',
+                        '{\n',
+                        'this ',
+                        [
+                            'tag',
+                            'is a {a test}\n',
+                            'okay',
+                        ],
+                        '\n'
+                     ],
+                    '}\n'
+                ],
+                '}\n'
+            ]
+        ])
+
+
+
+
